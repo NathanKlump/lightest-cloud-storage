0a307cd713bf2c3638b031e925a3739ca357a60d
NathanKlump <nathanieljklump@gmail.com>
2025-01-19 19:48:58 -0500

    first working counter

--- server/server.go
+++ server/server.go
@@ -1,16 +1,40 @@
+// main.go
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
 )
 
-func handler ( writer http.ResponseWriter, response *http.Request ) {
-	fmt.Fprint(writer, "hey there: ", response.URL.Path[1:])
+type PageData struct {
+	Count int
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Initialize counter
+	count := 0
+
+	// Serve static files
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Main page handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("client/index.html"))
+		data := PageData{Count: count}
+		tmpl.Execute(w, data)
+	})
+
+	// Increment counter endpoint
+	http.HandleFunc("/increment", func(w http.ResponseWriter, r *http.Request) {
+		count++
+		// Add a small delay to make the update visible
+		time.Sleep(100 * time.Millisecond)
+		fmt.Fprintf(w, "<div id='count'>%d</div>", count)
+	})
+
+	fmt.Println("Server starting on :8080...")
+	http.ListenAndServe(":8080", nil)
 }
